a5adb39ede79eb27d166c1a77af524f68148d7d7
Josh Perry <joshperry@outlook.com>
2025-07-07 21:29:10 +0100

    read nds files from zips

--- src/rom_hashes.rs
+++ src/rom_hashes.rs
@@ -1,7 +1,10 @@
 // https://docs.retroachievements.org/developer-docs/game-identification.html
 
-use std::io::{Read, Seek, SeekFrom};
+use std::{fs::{File}, io::{Read, Seek, SeekFrom}};
 use md5;
+
+trait ReadSeek: Read + Seek {}
+impl<T: Read + Seek> ReadSeek for T {}
 
 pub fn get_hash_function(system: &str) -> Option<fn(&str) -> Result<String, Box<dyn std::error::Error>>> {
     match system {
@@ -12,7 +15,7 @@ pub fn get_hash_function(system: &str) -> Option<fn(&str) -> Result<String, Box<
 }
 
 fn calculate_whole_file_hash(file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let mut file = std::fs::File::open(file_path)?;
+    let mut file = File::open(file_path)?;
     let mut hasher = md5::Context::new();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer)?;
@@ -27,10 +30,41 @@ fn calculate_nds_file_hash(file_path: &str) -> Result<String, Box<dyn std::error
     // The arm9 code address is stored at $20 in the header, and the size is stored at $2C in the header
     // The arm7 code address is stored at $30 in the header, and the size is stored at $3C in the header
 
-    let mut file = std::fs::File::open(file_path)?;
+    if !file_path.ends_with(".nds") && !file_path.ends_with(".zip") {
+        return Err("File is not a valid NDS ROM or ZIP archive".into());
+    }
+
+    fn get_nds_file(file_path: &str) -> Result<Box<dyn ReadSeek>, Box<dyn std::error::Error>> {
+        if file_path.ends_with(".zip") {
+            let mut zip_file_archive = zip::ZipArchive::new(File::open(file_path)?)?;
+
+            let file_names: Vec<String> = zip_file_archive.file_names().map(|name| name.to_string()).collect();
+            for file_name in file_names {
+                if file_name.ends_with(".nds") {
+                    // TODO: figure out how to do this without copying the file contents into a buffer
+                    // This is necessary (???) because the zip_file_archive dies before we read the contents down below.
+                    let mut file = zip_file_archive.by_name(&file_name)?;
+                    let mut buffer = Vec::new();
+                    file.read_to_end(&mut buffer)?;
+                    return Ok(Box::new(std::io::Cursor::new(buffer)));
+                }
+            }
+
+            return Err("No valid NDS file found in the ZIP archive".into());
+        } else {
+            return Ok(Box::new(File::open(file_path)?));
+        }
+    }
+
+    let mut nds_file = get_nds_file(file_path)?;
+    
+    if nds_file.seek(SeekFrom::Start(0)).is_err() {
+        print!("Failed to seek to the start of the file: {}", file_path);
+        return Err("Failed to seek to the start of the file".into());
+    }
 
     let mut header = [0; 0x160];
-    file.read_exact(&mut header)?;
+    nds_file.read_exact(&mut header)?;
 
     let icon_title_offset = u32::from_le_bytes(header[0x68..0x6C].try_into()?) as u64;
     let icon_title_size = 0xA00;
@@ -41,9 +75,9 @@ fn calculate_nds_file_hash(file_path: &str) -> Result<String, Box<dyn std::error
     let arm7_offset = u32::from_le_bytes(header[0x30..0x34].try_into()?) as u64;
     let arm7_size = u32::from_le_bytes(header[0x3C..0x40].try_into()?) as usize;
 
-    let icon_title_data = read_file_section(&mut file, icon_title_offset, icon_title_size)?;
-    let arm9_data = read_file_section(&mut file, arm9_offset, arm9_size)?;
-    let arm7_data = read_file_section(&mut file, arm7_offset, arm7_size)?;
+    let icon_title_data = read_file_section(&mut nds_file, icon_title_offset, icon_title_size)?;
+    let arm9_data = read_file_section(&mut nds_file, arm9_offset, arm9_size)?;
+    let arm7_data = read_file_section(&mut nds_file, arm7_offset, arm7_size)?;
 
     let mut hasher = md5::Context::new();
     hasher.consume(&header);
@@ -54,7 +88,7 @@ fn calculate_nds_file_hash(file_path: &str) -> Result<String, Box<dyn std::error
     Ok(format!("{:x}", hasher.finalize()))
 }
 
-fn read_file_section(file: &mut std::fs::File, offset: u64, size: usize) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+fn read_file_section(file: &mut impl ReadSeek, offset: u64, size: usize) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let mut buffer = vec![0; size];
     file.seek(SeekFrom::Start(offset))?;
     file.read_exact(&mut buffer)?;
